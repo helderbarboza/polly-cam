380155c73b729d2b17706042633b3b75757bf26e
Helder Barboza <helder.fontes@sicoob.com.br>
2024-03-12 15:45:50 -0400

    camera wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import CameraPhoto, * as CameraLib from "jslib-html5-camera-photo";
+	const { FACING_MODES, IMAGE_TYPES } = CameraLib;
+	let videoElement = document.getElementById("videoId") as HTMLVideoElement;
+	let cameraPhoto = new CameraPhoto(videoElement);
+
+	cameraPhoto
+		.startCamera(FACING_MODES.ENVIRONMENT, {})
+		.then((stream) => {
+			console.log("camera is started !");
+		})
+		.catch((error) => {
+			console.error("Camera not started!", error);
+		});
+</script>
+
+<pre>{JSON.stringify({}, null, 2)}</pre>
+
+<video id="videoId" autoplay={true}> <track kind="captions" /></video>
